ccd46da1251ee86f50addb42525a2683ce83901e
MadeYoga <vngla21@gmail.com>
2022-07-09 16:55:44 +0800

    Remove minify.sh

--- minify.sh
+++
@@ -1,3 +0,0 @@
-uglifyjs src/chunchunmaru-mde.js -m --no-annotations -c -o dist/chunchunmaru-mde.min.js
-uglifycss src/chunchunmaru-mde-default.css --ugly-comments --output dist/chunchunmaru-mde-default.min.css
-uglifycss src/chunchunmaru-mde-dracula.css --ugly-comments --output dist/chunchunmaru-mde-dracula.min.css
