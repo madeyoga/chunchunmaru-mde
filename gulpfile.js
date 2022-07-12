6aba3bd495f66bf8968fce7d5d96452d6282a87b
MadeYoga <vngla21@gmail.com>
2022-07-12 15:27:18 +0800

    gulpfile: rename output

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const { src, dest, watch, series } = require('gulp')
 
 const sass = require('gulp-sass')(require('sass'))
+const rename = require('gulp-rename');
 
 function buildStyles() {
   return src('./src/styles/index.scss')
     .pipe(sass())
+    .pipe(rename('chunmde.css'))
     .pipe(dest('./dist'))
 }
 
