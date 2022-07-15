b7181e0e5bf9b54134b0511762c651546a50d691
MadeYoga <vngla21@gmail.com>
2022-07-15 20:17:23 +0800

    Add new commands to default extensions

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,16 @@ import { markdown, markdownLanguage } from "@codemirror/lang-markdown"
 import { EditorView, basicSetup } from 'codemirror'
 import { EditorState, EditorStateConfig, Extension } from '@codemirror/state'
 import { keymap, ViewUpdate } from '@codemirror/view'
-import { italicKeyBinding } from './commands/italic'
-import { boldKeyBinding } from "./commands/bold"
+import { italicKeyBinding } from './commands/Italic'
+import { boldKeyBinding } from "./commands/Bold"
+import { codeKeyBinding } from "./commands/Code"
+import { linkKeyBinding } from "./commands/Link"
+import { quoteKeyBinding } from "./commands/Quote"
+import { ulKeyBinding } from "./commands/BulletedList"
 
 interface ChunInterface {
   editor: EditorView,
+  getValue: () => string,
 }
 
 interface ChunConfig extends EditorStateConfig {
@@ -17,10 +22,14 @@ function ChunMDE(this: ChunInterface, containerId: string, customConfig?: ChunCo
   const defaultExtensions = [
     keymap.of([
       italicKeyBinding,
-      boldKeyBinding
+      boldKeyBinding,
+      codeKeyBinding,
+      linkKeyBinding,
+      quoteKeyBinding,
+      ulKeyBinding,
     ]),
-    basicSetup,
     markdown({ base: markdownLanguage }),
+    basicSetup,
   ]
 
   let config: EditorStateConfig = {
@@ -36,10 +45,16 @@ function ChunMDE(this: ChunInterface, containerId: string, customConfig?: ChunCo
     config.extensions = customConfig.extensions ? customConfig.extensions : defaultExtensions
   }
 
+  /** CodeMirror6's EditorView */
   this.editor = new EditorView({
     parent: document.getElementById(containerId) as Element,
     state: EditorState.create(config)
   })
+
+  /** Shortcut to get the editor value */
+  this.getValue = () => {
+    return this.editor.state.doc.toString()
+  }
 }
 
 declare global {
